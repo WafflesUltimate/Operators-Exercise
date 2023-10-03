1bc5d30d5a9ca16c0242e4123faf4022a8a9e076
David Armstrong <wafflesultimate1@gmail.com>
2023-10-02 19:21:20 -0700

    Completed Exercises 1 and 2

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -2,9 +2,54 @@
 {
     public class Program
     {
+
+        static void Addition(int a, int b)
+        {
+            int result = a + b;
+            Console.WriteLine($"{a}+{b} is {result}.");
+        }
+
+        static void Subtraction(int a, int b)
+        {
+            int result = a - b;
+            Console.WriteLine($"{a}-{b} is {result}.");
+        }
+
+        static void Multiplication(int a, int b)
+        {
+            int result = a * b;
+            Console.WriteLine($"{a}*{b} is {result}.");
+        }
+
+        static void Division(int a, int b)
+        {
+            int quotient = a / b;
+            int remainder = a % b;
+            Console.WriteLine($"{a}/{b} is {quotient}. The remainder is {remainder}");
+        }
+
+        static void AreaOfCircle()
+        {
+            Console.WriteLine("What is the radius of your circle?");
+            double radius = double.Parse(Console.ReadLine());
+            double areaOfCircle = Math.PI* Math.Pow(radius, 2);
+            Console.WriteLine($"The area of a circle, with the radius of {radius}, is {areaOfCircle}.");
+        }
+
+
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Addition(1, 3);
+
+            Subtraction(10, 5);
+
+            Multiplication(9, 5);
+
+            Division(20, 5);
+
+            AreaOfCircle();
+
         }
     }
 }
